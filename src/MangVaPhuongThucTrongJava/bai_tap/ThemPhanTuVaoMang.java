efad2cb192c35bc3540a5f175c8b3f0ce5d02c74
Your Name <khanh06111993@gmail.com>
2020-07-01 10:58:34 +0700

    01/07/2020

---
+++ src/MangVaPhuongThucTrongJava/bai_tap/ThemPhanTuVaoMang.java
@@ -0,0 +1,51 @@
+package MangVaPhuongThucTrongJava.bai_tap;
+
+import java.util.Scanner;
+
+public class ThemPhanTuVaoMang {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter a size: ");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20");
+            }
+        } while (size > 20);
+
+        array = new int[size + 1];
+        int i = 0;
+        while (i < array.length - 1) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+
+        }
+        System.out.print("Property list: ");
+        System.out.println("");
+        for (int j = 0; j < array.length - 1; j++) {
+            System.out.println(array[j] + "\t");
+        }
+        System.out.println("Nhap so can them: ");
+        int input_name = scanner.nextInt();
+        System.out.println("nhap vi tri can chen " + input_name + " vao trong mang");
+        int index_del = scanner.nextInt();
+
+        for (i = index_del - 1; i < array.length-1; i++) {
+
+            array[i] = array[i+1];
+        }
+        array[index_del-1]=input_name;
+
+
+        for (int j = 0; j < array.length; j++) {
+            System.out.println("\t" + array[j] + "\t");
+
+        }
+    }
+}
+
--- src/vong_lap_trong_java/bai_tap/HienThiCacLoaiHinh.java
+++ src/vong_lap_trong_java/bai_tap/HienThiCacLoaiHinh.java
@@ -10,9 +10,53 @@ public class HienThiCacLoaiHinh {
                 " 4 different angles: top-left, top-right, botton-left, botton-right)");
         System.out.println("3.Print isosceles triangle");
         System.out.println("4.Exit");
-        int choise=scanner.nextInt();
-        switch (choise){
+        System.out.println("Enter number: ");
+        int choise = scanner.nextInt();
+        switch (choise) {
             case 1:
+                for (int i = 0; i < 3; i++) {
+                    System.out.println();
+                    for (int j = 0; j < 7; j++) {
+                        System.out.print("*");
+                    }
+                }
+                break;
+            case 2: {
+                for (int i = 0; i < 6; i++) {
+                    System.out.println();
+                    for (int j = 0; j < i; j++) {
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+
+                for (int i = 5; i > 0; i--) {
+                    System.out.println();
+                    for (int j = 0; j < i; j++) {
+                        System.out.print("*");
+                    }
+                }
+                break;
+            }
+            case 3:
+                System.out.println("Nhập chiều dài cạnh đáy:");
+                int number=scanner.nextInt();
+                for (int i = 0; i < number; i++) {
+                    System.out.println();
+                    for (int k = 0; k < (number+1) - i; k++) {
+                        System.out.print(" ");
+                    }
+                    for (int j = 1; j <= (2*i+1); j++) {
+                        System.out.print("*");
+                    }
+
+
+                }
+                break;
+
+
         }
     }
 }
+
+
