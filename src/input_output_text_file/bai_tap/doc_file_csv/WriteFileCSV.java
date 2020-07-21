aca9bb804b47f8a5ec1ec24577aa0b1ba02453db
Your Name <khanh06111993@gmail.com>
2020-07-21 23:51:22 +0700

    night 21/07/2020 23h50

---
+++ src/input_output_text_file/bai_tap/doc_file_csv/WriteFileCSV.java
@@ -0,0 +1,38 @@
+package input_output_text_file.bai_tap.doc_file_csv;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class WriteFileCSV {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap duong dan cua danh ba can doc:");
+        String fileCSV= scanner.nextLine();
+        WriteFileCSV country=new WriteFileCSV();
+        country.writeFileCSV(fileCSV);
+    }
+
+
+
+    public void writeFileCSV(String fileCSV){
+        try{
+            File file=new File(fileCSV);
+            FileReader fileReader=new FileReader(file);
+            BufferedReader br=new BufferedReader(fileReader);
+            String line = "";
+            String cvsSplitBy = ",";
+            int i = 0;
+            while ((line=br.readLine())!=null){
+                String[] country=line.split(cvsSplitBy);
+                System.out.println("Country "+(i+1)+" is: \t"+country[5]);
+                i++;
+            }
+
+        } catch (Exception e){
+            System.out.println("Error");
+        }
+
+    }
+}
---
+++ src/input_output_text_file/thuc_hanh/Test.java
@@ -0,0 +1,26 @@
+package input_output_text_file.thuc_hanh;
+
+import java.io.*;
+
+public class Test {
+        public static void main(String[] args){
+//           File fileWriter =new File("src/input_output_text_file/bai_tap/ahuhu.txt");
+//            fileWriter.createNewFile();
+try{
+    File file= new File("src/input_output_text_file/bai_tap/test.txt");
+    File file1= new File("src/input_output_text_file/bai_tap/copy_test.txt");
+    file1.createNewFile();
+    file.createNewFile();
+    PrintWriter writer= new PrintWriter(file);
+    writer.println("aaa");
+    writer.println("bbb");
+    writer.println("ccc");
+    writer.close();
+
+}catch (Exception e){
+    System.out.println("Chua co file");
+}
+
+
+        }
+}
