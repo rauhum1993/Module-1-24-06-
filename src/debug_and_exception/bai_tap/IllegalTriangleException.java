ab4e95c965d09cea472435e00bd3b6475fd6bb8f
Your Name <khanh06111993@gmail.com>
2020-07-21 15:55:51 +0700

    evening 21/07/2020 16h00

--- src/debug_and_exception/bai_tap/IllegalTriangleException.java
+++ src/debug_and_exception/bai_tap/IllegalTriangleException.java
@@ -7,15 +7,6 @@ public class IllegalTriangleException  extends  Exception{
         super(message);
     }
 
-    public static void checkException(int a, int b, int c)throws IllegalTriangleException {
-        if (a < 0 || b < 0 || c < 0) {
-            throw new IllegalTriangleException("Nhap sai ,canh be hon 0");
-        }
-        if (a + b < c || b + c < a || a + c < b) {
-            throw new IllegalTriangleException("Nhap tong hai canh be hon canh con lai");
-        }
-
-    }
 
 }
 
--- src/input_output_text_file/thuc_hanh/WriteATextFile.java
+++ src/input_output_text_file/thuc_hanh/WriteATextFile.java
@@ -1,11 +1,15 @@
 package input_output_text_file.thuc_hanh;
 
+import java.io.File;
 import java.io.IOException;
 
 public class WriteATextFile {
     public static void main(String[] args) {
-//        try{
-////
-////        } catch (IOException e){}
+     File file= new File("quang love mai.txt");
+     if(!file.isFile()){
+         System.out.println("not file");
+//         file.createNewFile("anh chuong love mai.txt");
+     }
+
     }
 }
