43b4444e766cef9a557f64ee7c3777d7dc96ab78
Your Name <khanh06111993@gmail.com>
2020-08-17 14:39:24 +0700

    14h40 17/08/2020

--- module3/lesson 2_thiet_ke_va_tao_CSDL/practice/Truy_van_du_lieu_nang_cao.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists classicmodels;
-create database classicmodels;
-use classicmodels;
-
-create table productlines (
-productline varchar not null primary key,
-textDescription 
-
-);
--- module3/lesson3_thao_tac_voi_CSDL/practice/Truy_van_du_lieu_nang_cao.sql
+++ module3/lesson3_thao_tac_voi_CSDL/practice/Truy_van_du_lieu_nang_cao.sql
@@ -3,7 +3,22 @@ create database classicmodels;
 use classicmodels;
 
 create table productlines (
-productline varchar not null primary key,
-textDescription 
+productline char not null primary key,
+text_description varchar(255),
+html_descriptinon varchar(255),
+image varchar(255)
+
+);
+create table products (
+product_code char  primary key ,
+prduct_name char,
+product_scale char,
+product_vendor char,
+product_description char,
+quantityln_stock int ,
+buy_price double,
+MSRP char,
+product_line char ,
+foreign key(product_line)references productlines(productline)
 
 );
