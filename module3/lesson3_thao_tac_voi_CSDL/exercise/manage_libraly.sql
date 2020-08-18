fb0b8864f2e62926ffe5b230386c1152d286f93c
Your Name <khanh06111993@gmail.com>
2020-08-18 09:48:35 +0700

    19h45 18/08/2020

--- module3/lesson3_thao_tac_voi_CSDL/exercise/manage_libraly.sql
+++ module3/lesson3_thao_tac_voi_CSDL/exercise/manage_libraly.sql
@@ -1,25 +1,40 @@
 drop database if exists manage_libraly;
 create database manage_libraly;
-
 use manage_libraly;
+
+create table category(
+	category_id int primary key,
+    category_name varchar(500) 	
+);
+
 create table books(
-	book_code char ,
+	book_id int  primary key,
     book_name char,
     producer char,
     author char,
     year_of_manufacture year,
     price_book double check (price_book>0) ,
     number_of_publications int 	check (number_of_publications >1 ),
-    image varchar(500)
+    image varchar(500),
+    category_id int not null,
+    foreign key (category_id) references manage_libraly.category(category_id)
     
 );
 create table students (
-	student_number varchar(15) not null primary key,
+	student_id int primary key ,
     student_name varchar(50),
     address varchar(500),
      email varchar (50),
     image varchar(500)
 );
 
-create table borrow_order;
-create table category;
+create table borrow_order(
+	student_id int  ,
+    foreign key (student_id) references manage_libraly.students(student_id),
+    book_id int ,
+    foreign key(book_id) references manage_libraly.books(book_id),
+    date_borrow date,
+    date_return date
+);
+
+
