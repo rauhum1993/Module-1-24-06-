91b5d80034b7ecf470f562c133c4b44ec07bdc1f
Your Name <khanh06111993@gmail.com>
2020-08-19 09:47:47 +0700

    a

--- module3/lesson4_cac_ham_thong_dung_trong_SQL/exercise/ham_thong_dung_trong_SQL.sql
+++ module3/lesson4_cac_ham_thong_dung_trong_SQL/exercise/ham_thong_dung_trong_SQL.sql
@@ -17,15 +17,31 @@ insert into course2020 (id,student_name,age,course,price) values (4,'Nhan',19,'C
 insert into course2020 (id,student_name,age,course,price) values (5,'Huong',20,'TCNH',500000);
 insert into course2020 (id,student_name,age,course,price) values (5,'Huong',20,'TCNH',200000);
 
-select*from code_gym_course.course2020;
-select *from code_gym_course.course2020 
+-- 1.Hiển thị toàn bộ bảng-- 
+select*
+from code_gym_course.course2020;
+
+-- 2.Viết câu lệnh hiện thị tất cả các dòng có tên là Huong-- 
+select *
+from code_gym_course.course2020 
 where course2020.student_name='Huong';
 
-select student_name, sum(price) as sum_price from course2020
+-- 3. Viết câu lệnh lấy ra tổng số tiền của Huong-- 
+select student_name, sum(price) as sum_price 
+from course2020
 where student_name='Huong';
 
-select student_name from course2020 
+-- 4.Viết câu lệnh lấy ra tên danh sách của tất cả học viên, không trùng lặp-- 
+
+-- cách 1: sử dụng group by.-- 
+select student_name 
+from course2020 
 group by student_name;
+
+-- cách 2: sử dụng distinct-- 
+select distinct student_name
+from course2020;
+
 
 
 
